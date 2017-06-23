11bed3a47d6c84c3d27984f9f4b2b56c9884a7e7
sunnydocker <462720846@qq.com>
2017-06-23 15:07:57 +0800

    django tutorial init

---
+++ myblog/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+import os
+
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'myblog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$','article.views.home'),
+    # url(r'^(?P<my_args>\d+)/$','article.views.detail',name = 'detail'),
+    # url(r'^$','article.views.test'),
+
+
+)
+
+
+
